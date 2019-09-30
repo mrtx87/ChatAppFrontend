92de66136afc356f96b5ecf30d2d6f0027dc95ae
Patrick Beiersdörfer <patrick.beiersdoerfer@arxes-tolina.de>
2019-09-30 21:04:02 +0200

    LISTE MIT IDEEN

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,11 +10,11 @@ import { User } from '../Entities/user';
 })
 export class RoomsComponent implements OnInit {
 
-  
-  get availableRooms() : Map<string, ChatRoom> {
+
+  get availableRooms(): Map<string, ChatRoom> {
     return this.chatService.availableRooms;
   }
-  set availableRooms(val : Map<string, ChatRoom>){
+  set availableRooms(val: Map<string, ChatRoom>) {
     this.chatService.availableRooms = val;
   }
 
@@ -24,14 +24,14 @@ export class RoomsComponent implements OnInit {
   set displayedChatRoom(val: ChatRoom) {
     this.chatService.displayedChatRoom = val;
   }
-  
+
   get localUser(): User {
     return this.chatService.localUser;
   }
   set localUser(val: User) {
     this.chatService.localUser = val;
   }
-  
+
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
@@ -43,5 +43,40 @@ export class RoomsComponent implements OnInit {
     this.chatService.sendUpdateSeenMessages(chatRoom.unseenChatMessageIds);
     chatRoom.unseenChatMessageIds = null;
   }
+
+
+  //Gruppe/Raum mit mehreren Nutzern aus Nutzerliste hinzufügen (nur lokale nutzer)
+
+  // wenn Gruppenmitglieder nicht in der eigenen Kontaktliste dann nur Stub (nur benutzernamen ohne verlinmkung zum proofil)
+  // anzeigen
+
+  // eventue
+
+  // Suche in Chatroom Messages bauen, lokale suche und backend-suche
+  // springe zum last found matching entry (wenn last found matching entry zu weit weg dann füge
+  // nur patch der daten ein) Gedanken machen über übergang und grundsätzliches scroll handling
+
+  // Handling wenn man Scrollt sollen nachrichten nachgeladen werden
+
+  // Parser zum Auflösen aller variablen innerhalb angezeigter Texte
+
+  // Box bauen aus der man Icons auswählen kann
+  // eventuell  nach zeichen -> icon map suchen. 
+  // das heißt eine datenquelle die für =) das entprechende Icon auflösen kann
+
+
+  // Bilder einfügbar machen und croppen ( grö0e reduzieren)
+  
+  // Bilder speichern Gedanken machen z.B. Base64 String in DB, als Blob in DB oder als Datei
+  // im DAteisystem
+
+  //Retrieving 100 lastest messages per room.
+
+  //layout für nachrichten an Whatsapp anpassen
+
+  // gedanken machen wie man das Datum vor allen 
+  // nachrichten des entsprechenden datums ermitteln und nazeigen kann
+
+
 
 }
