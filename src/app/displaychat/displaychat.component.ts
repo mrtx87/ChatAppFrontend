5df4c5e4922dcbd405d83a1594b98886aeb0750a
Patrick Beiersdörfer <patrick.beiersdoerfer@arxes-tolina.de>
2019-10-07 00:35:28 +0200

    filereader added with basic functionality

--- src/app/displaychat/displaychat.component.ts
+++ src/app/displaychat/displaychat.component.ts
@@ -29,6 +29,8 @@ export class DisplaychatComponent implements OnInit {
     this.chatService.localUser = val;
   }
 
+
+
   get currentDisplayMessages(): ChatMessage[] {
     if (this.chatService.chatMessagesByRoom && this.displayedChatRoom) {
       return this.chatService.chatMessagesByRoom.get(this.displayedChatRoom.id);
@@ -36,7 +38,52 @@ export class DisplaychatComponent implements OnInit {
     return [];
   }
 
-  constructor(private chatService: ChatService, private constants: Constants, private store : DataStore) { }
+
+  imgsrc;
+  img;
+  onFileChanged(event) {
+    const file = event.target.files[0]
+
+    // Create an image
+    this.img = document.createElement("img");
+    // Create a file reader
+    let reader = new FileReader();
+    let that = this;
+    // Set the image once loaded into file reader
+    reader.onload = function (e) {
+      let res : any = e.target;
+      that.downscaleImage(res.result, 100, "image/jpeg", 0.7);
+    }
+    // Load files into file reader
+    reader.readAsDataURL(file);
+
+  }
+
+  downscaleImage(dataUrl, size, imageType, imageArguments) {
+    let image, canvas, ctx, newDataUrl;
+
+    // Provide default values
+    imageType = imageType || "image/jpeg";
+    imageArguments = imageArguments || 0.7;
+
+    // Create a temporary image so that we can compute the height of the downscaled image.
+    image = new Image();
+    image.src = dataUrl;
+    let that = this;
+    image.onload = function() {
+        // Create a temporary canvas to draw the downscaled image on.
+        canvas = document.createElement("canvas");
+        canvas.width = 100;
+        canvas.height = 100;
+        // Draw the downscaled image on the canvas and return the new data URL.
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, size, size);
+        newDataUrl = canvas.toDataURL(imageType, imageArguments);
+        that.imgsrc = newDataUrl;
+    }
+  }
+
+  constructor(private chatService: ChatService, private constants: Constants, private store: DataStore) { }
 
   ngOnInit() {
   }
