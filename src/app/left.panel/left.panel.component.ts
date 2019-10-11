c2a84f556e0d4a6da70d3d1d8d8dffd6d81159c0
Patrick Beiersdörfer <patrick.beiersdoerfer@arxes-tolina.de>
2019-10-11 18:16:06 +0200

    filter auf rooms

--- src/app/left.panel/left.panel.component.ts
+++ src/app/left.panel/left.panel.component.ts
@@ -14,6 +14,8 @@ import * as moment from 'moment';
 })
 export class LeftPanelComponent implements OnInit {
 
+
+  searchChatInputText: string = "";
   uploadImage: string;
 
   displayUserMenu: boolean = false;
@@ -47,7 +49,7 @@ export class LeftPanelComponent implements OnInit {
         return dateB - dateA;
       }
     })
-    return availableRooms;
+    return availableRooms.filter(room => this.values.resolveChatRoomName(room).includes(this.searchChatInputText));
   }
 
   get displayedChatRoom(): ChatRoom {
