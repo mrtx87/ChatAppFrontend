52619d689a7402f3d9b588aea2def26a4fe629aa
SirLock <gedanke@quantentunnel.de>
2019-10-19 00:47:26 +0200

    Prototyp für removeContact feature

--- src/app/left.panel/left.panel.component.ts
+++ src/app/left.panel/left.panel.component.ts
@@ -87,6 +87,25 @@ export class LeftPanelComponent implements OnInit {
       chatRoom.unseenChatMessageIds = null;
     }
   }
-
+  
+  removeContact(chatRoom: ChatRoom){
+    let otherUser = this.getOtherUser(chatRoom.userIds);
+    if(otherUser!=="" || otherUser != null){
+      console.log("Ya rly want to remove "+ this.store.lookUpInDATA(otherUser).name +", huh? Well, guess we'll have to work on that.");
+    }else{
+      console.log("Don't remove anything as there was no other user. (Which is strange. You should investigate this.");
+    }
+  }
+ 
+  private getOtherUser(userIds: string[]): string{
+    if(userIds.length == 2){
+      if(userIds[0] == this.localUser.id){
+        return userIds[1];
+      }else{
+        return userIds[0];
+      }
+    }
+    return null;
+  }
 
 }
